9fec20db4653e463cba0b9d24b3d3f3819b7aa16
Михаил <mishafedoseev753@gmail.com>
2026-07-12 17:00:51 +0500

    init: установлены зависимости, определена структура проекта

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,37 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      globals: globals.browser,
+    globalIgnores(["dist"]),
+    {
+        files: ["**/*.{ts,tsx}"],
+        extends: [
+            js.configs.recommended,
+            tseslint.configs.recommended,
+            reactHooks.configs.flat.recommended,
+            reactRefresh.configs.vite,
+        ],
+        languageOptions: {
+            globals: globals.browser,
+        },
     },
-  },
-])
+    {
+        rules: {
+            "prettier/prettier": "off",
+
+            "object-curly-newline": [
+                "tip",
+                {
+                    ImportDeclaration: { multiline: true, minProperties: 2 },
+                    ExportDeclaration: { multiline: true, minProperties: 2 },
+                },
+            ],
+
+            "object-curly-spacing": ["tip", "always"],
+        },
+    },
+]);
